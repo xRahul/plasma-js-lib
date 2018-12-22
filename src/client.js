e9f16d598ee6f338f5806a6ae579baaee0c5e4ed
Kelvin Fichter <kelvinfichter@gmail.com>
2018-12-22 13:33:51 -0500

    Initial commit

---
+++ src/client.js
@@ -0,0 +1,23 @@
+class PlasmaClient {
+  sign (transaction) {
+    throw Error('Not implemented')
+  }
+
+  sendTransaction (transaction) {
+    throw Error('Not implemented')
+  }
+
+  getBalance () {
+    throw Error('Not implemented')
+  }
+
+  startExit () {
+    throw Error('Not implemented')
+  }
+
+  getHistory (range, start, end) {
+    throw Error('Not implemented')
+  }
+}
+
+module.exports = PlasmaClient
